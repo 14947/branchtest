3b4345f1c58dfb93b05f8fe06bb62b3e6dd58ece
dan 14947 <14947@coderacademy.edu.au>
2023-11-30 20:49:48 +1100

    first branch test

---
+++ division.py
@@ -0,0 +1,17 @@
+
+
+n = int(input("Enter numerator: "))
+d = int(input("Enter denominator: "))
+
+# if d === 0
+if d == 0:
+    print("Denominator cannot be zero")
+    # display 'denominator cannot be zero' as output
+
+
+else:
+    q = n / d
+    print(q)
+# else
+    # d = n / d
+    # display q
